22f79928983cbec3382030922ce7f53f134fc8bb
zouhao <zouhao@antiy.cn>
2017-02-16 11:07:51 +0800

    add mongopipelines

--- app/app/pipelines/pipelines.py
+++ app/app/pipelines/pipelines.py
@@ -5,7 +5,16 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import pymongo
+from scrapy.conf import settings
 
-class AppPipeline(object):
+class MongoPipeline(object):
+
+    def __init__(self):
+        self.client = pymongo.MongoClient(settings['MONGO']['default']['host'], settings['MONGO']['default']['port'])
+        self.database = self.client[settings['MONGO']['default']['database']]
+        self.collection = self.database[settings['MONGO']['default']['collection']]
+
     def process_item(self, item, spider):
+        self.collection.insert(dict(item))
         return item
