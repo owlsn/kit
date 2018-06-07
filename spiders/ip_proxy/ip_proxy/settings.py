34acc9c0af5976ab4f12b9ce32100a0eecc59d60
owlsn <helloworld.zouhao@gmail.com>
2018-06-07 20:58:25 +0800

    setting user-agent

--- spiders/ip_proxy/ip_proxy/settings.py
+++ spiders/ip_proxy/ip_proxy/settings.py
@@ -8,7 +8,7 @@
 #     https://doc.scrapy.org/en/latest/topics/settings.html
 #     https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://doc.scrapy.org/en/latest/topics/spider-middleware.html
-
+import random
 BOT_NAME = 'ip_proxy'
 
 SPIDER_MODULES = ['ip_proxy.spiders']
@@ -16,7 +16,17 @@ NEWSPIDER_MODULE = 'ip_proxy.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'ip_proxy (+http://www.yourdomain.com)'
+USER_AGENT_LIST = (
+    'User-Agent,Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.62 Safari/537.36',
+    'User-Agent,Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50',
+    'User-Agent,Mozilla/5.0 (Windows; U; Windows NT 6.1; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50',
+    'User-Agent, Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv,2.0.1) Gecko/20100101 Firefox/4.0.1',
+    'User-Agent,Mozilla/5.0 (Windows NT 6.1; rv,2.0.1) Gecko/20100101 Firefox/4.0.1',
+    'User-Agent,Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; en) Presto/2.8.131 Version/11.11',
+    'User-Agent,Opera/9.80 (Windows NT 6.1; U; en) Presto/2.8.131 Version/11.11',
+    'User-Agent, Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11'
+    )
+USER_AGENT = random.sample(USER_AGENT_LIST, 1)[0]
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
