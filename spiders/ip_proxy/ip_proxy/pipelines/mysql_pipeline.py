6f04da4c49598b37761fcf091b0d80f89e2841d7
owlsn <helloworld.zouhao@gmail.com>
2018-06-07 14:22:52 +0800

    mv database

--- spiders/ip_proxy/ip_proxy/pipelines/mysql_pipeline.py
+++ spiders/ip_proxy/ip_proxy/pipelines/mysql_pipeline.py
@@ -5,34 +5,37 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
-from ip_proxy.database.mysql_helper import MysqlHelper
 from ip_proxy.config import *
+import pymysql
 import time
 import json
 import traceback
 
 class MysqlPipeline(object):
+
+    def __init__(self):
+        self.config = MYSQL
+        self.conn = pymysql.connect(host = self.config['host'], 
+        user = self.config['user'], password = self.config['passwd'], 
+        database = self.config['database'], port = self.config['port'], 
+        charset = self.config['charset'])
+        pass
+
+
     def process_item(self, item, spider):
-        mysql = MysqlHelper()
-        conn = mysql.conn
-        cursor = conn.cursor()
+        cursor = self.conn.cursor()
         try:
             sql = "insert into `ip` (`ip`, `isp`, `country`, `region`, `city`, `area`, `create_time`) values ('%s', '%s', '%s', '%s', '%s', '%s', %f);" % (item['ip'], item['isp'], item['country'], item['region'], item['city'], item['area'], time.time())
             res = cursor.execute(sql)
-            print(sql)
+            #print(sql)
             if res != 1:
-                with open(LOG_PATH, 'a') as f:
-                    f.writelines('item:' + json.dumps(dict(item)) + "\n")
-                    f.write('sql:' + sql + "\n")
-                f.close()
+                raise Exception('insert failed!');
             else :
-                conn.commit()
+                self.conn.commit()
         except Exception as e:
             with open(LOG_PATH, 'a') as f:
                 f.write('exception:' + traceback.format_exc() + "\n")
                 f.write('item:' + json.dumps(dict(item)) + "\n")
                 f.write('sql:' + sql + "\n")
             f.close()
-        finally:
-            conn.close()
         return item
