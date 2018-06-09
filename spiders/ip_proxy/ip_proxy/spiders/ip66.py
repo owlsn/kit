d89cd006f50adc5a18452117fde088d56e193b80
owlsn <helloworld.zouhao@gmail.com>
2018-06-09 16:19:22 +0800

    optimize

--- spiders/ip_proxy/ip_proxy/spiders/ip66.py
+++ spiders/ip_proxy/ip_proxy/spiders/ip66.py
@@ -4,10 +4,11 @@ from ip_proxy.item.ip66_item import Ip66Item
 from ip_proxy.connection.redis_connection import RedisConnection
 import time
 from ip_proxy.config import LOG_PATH
+import json
 
 class Ip66Spider(scrapy.Spider):
     name = 'ip66'
-    base_url = 'http://www.66ip.cn'
+    base_url = 'http://www.66ip.cn/'
     allowed_domains = ['www.66ip.cn']
     start_urls = ['http://www.66ip.cn/']
 
@@ -18,19 +19,19 @@ class Ip66Spider(scrapy.Spider):
         pass
 
     def parse(self, response):
+        if response.url == self.base_url:
+            # 解析起始页面的头部标签的url
+            for li in response.xpath('//ul[@class="textlarge22"]/li'):
+                value = li.xpath('a/@href').extract()
+                if value and value[0].startswith('/', 0, 1):
+                    url = self.base_url + value[0]
+                    if not self.conn.get(url):
+                        self.conn.set(url, 1)
+                        yield self.make_requests_from_url(url)
         item = Ip66Item()
         # 解析分页的url
         for a in response.xpath('//div[@id="PageList"]/a'):
             value = a.xpath('@href').extract()
-            if value and value[0].startswith('/', 0, 1):
-                url = self.base_url + value[0]
-                if not self.conn.get(url):
-                    self.conn.set(url, 1)
-                    yield self.make_requests_from_url(url)
-
-        # 解析头部标签的url
-        for li in response.xpath('//ul[@class="textlarge22"]/li'):
-            value = li.xpath('a/@href').extract()
             if value and value[0].startswith('/', 0, 1):
                 url = self.base_url + value[0]
                 if not self.conn.get(url):
