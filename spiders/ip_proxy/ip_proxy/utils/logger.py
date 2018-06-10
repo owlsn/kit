9d095cb8c4712e8d80d49c016d37ccc7e0f5d0cc
owlsn <helloworld.zouhao@gmail.com>
2018-06-10 15:34:47 +0800

    log

--- spiders/ip_proxy/ip_proxy/utils/logger.py
+++ spiders/ip_proxy/ip_proxy/utils/logger.py
@@ -1,1 +1,17 @@
 # coding = utf-8
+
+from scrapy import logging
+
+class Logger(object):
+
+    defalut_path = ''
+
+    default_level = logging.INFO
+
+    def __init__(self):
+        if os.path.exists(default_path):
+            logging.config.dictConfig(default_path)
+        else:
+            logging.basicConfig(level = default_level)
+        pass
+    
