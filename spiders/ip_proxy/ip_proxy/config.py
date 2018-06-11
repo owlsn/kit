cb5f464c1c88ebd65cc8d33543a733adf62717ff
owlsn <helloworld.zouhao@gmail.com>
2018-06-11 09:31:59 +0800

    delete log

--- spiders/ip_proxy/ip_proxy/config.py
+++ spiders/ip_proxy/ip_proxy/config.py
@@ -1,10 +1,4 @@
 # coding = utf-8
-import os
-
-ROOT = os.path.dirname(os.path.realpath(__file__))
-LOG_PATH = ROOT + '/log/'
-if not os.path.exists(LOG_PATH):
-    os.mkdir(LOG_PATH)
 MYSQL = {
     'host' : '127.0.0.1',
     'port' : 3306,
