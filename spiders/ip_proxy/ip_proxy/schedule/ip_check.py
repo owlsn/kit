42cfb92ee580c4fafb914fc04579e5fcb351b5c1
owlsn <helloworld.zouhao@gmail.com>
2018-06-12 20:06:15 +0800

    ip_check

--- spiders/ip_proxy/ip_proxy/schedule/ip_check.py
+++ spiders/ip_proxy/ip_proxy/schedule/ip_check.py
@@ -10,34 +10,14 @@ import socket
 import struct
 
 if __name__ == '__main__':
-    
     tool = IpTools()
-
     mysql = MysqlConnection(type = 'syn')
-
     conn = mysql.conn
-
     cursor = conn.cursor()
-
     sql = """select ip,port from `ip` limit 0,10"""
-
     cursor.execute(sql)
-
     res = cursor.fetchall()
-
     for value in res:
-        
         ip = socket.inet_ntoa(struct.pack('I',socket.htonl(value[0])))
-
         port = value[1]
-        
         r = tool.ip_check(ip, port)
-
-        print(r)
-
-    # for li in cursor:
-        
-    #     print(123)
-        # res = tool.ip_check(li['ip'], li['port'])
-
-        # print(res)
