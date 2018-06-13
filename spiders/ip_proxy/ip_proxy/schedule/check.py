2e8094ab13b6939a3ebadde89c0dfb6c90e59e68
owlsn <helloworld.zouhao@gmail.com>
2018-06-13 21:07:49 +0800

    process

--- spiders/ip_proxy/ip_proxy/schedule/check.py
+++ spiders/ip_proxy/ip_proxy/schedule/check.py
@@ -3,7 +3,7 @@ import os
 import sys
 p = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.append(p)
-from scrapy.crawler import CrawlerRunner
+from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 from ip_proxy.spiders.check import CheckSpider
 from ip_proxy.utils.log import log
@@ -11,12 +11,11 @@ import traceback
 
 if __name__ == '__main__':
     try:
-        logger = log.getLogger('debug')
-        logger.debug('test 123')
         check = CheckSpider()
         settings = get_project_settings()
-        runner = CrawlerRunner(settings)
-        runner.crawl(check)
+        process = CrawlerProcess(settings)
+        process.crawl(check)
+        process.start()
     except Exception as e:
         logger = log.getLogger('debug')
         logger.debug(traceback.format_exc())
