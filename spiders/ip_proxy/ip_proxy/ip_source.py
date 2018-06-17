b5e29f7cb1b0df53c2d5a211b176d12994ee0a89
owlsn <helloworld.zouhao@gmail.com>
2018-06-17 19:52:09 +0800

    crawl

--- spiders/ip_proxy/ip_proxy/ip_source.py
+++ spiders/ip_proxy/ip_proxy/ip_source.py
@@ -3,6 +3,17 @@
 URL_LIST = [
     {
         'name' : 'ip66',
-        'module' : '',
+        'module' : 'ip_proxy.spiders.ip66',
+        'class' : 'Ip66Spider'
+    },
+    {
+        'name' : 'ip89',
+        'module' : 'ip_proxy.spiders.ip89',
+        'class' : 'Ip89Spider'
+    },
+    {
+        'name' : 'check',
+        'module' : 'ip_proxy.spiders.check',
+        'class' : 'CheckSpider'
     }
 ]
--- spiders/ip_proxy/ip_proxy/schedule/crawl.py
+++ spiders/ip_proxy/ip_proxy/schedule/crawl.py
@@ -3,18 +3,22 @@ import os
 import sys
 p = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.append(p)
+import importlib
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
-from ip_proxy.spiders.check import CheckSpider
+from ip_proxy.ip_source import URL_LIST
 from ip_proxy.utils.log import log
 import traceback
 
 if __name__ == '__main__':
     try:
-        check = CheckSpider()
         settings = get_project_settings()
         process = CrawlerProcess(settings)
-        process.crawl(check)
+        for value in URL_LIST:
+            ip_module = importlib.import_module(value['module'])
+            ip_module_cls = getattr(ip_module, value['class'])
+            cls_obj = ip_module_cls()
+            process.crawl(cls_obj)
         process.start()
     except Exception as e:
         logger = log.getLogger('debug')
