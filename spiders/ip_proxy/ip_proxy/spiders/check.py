387e1839f70774a2a7284bbfde23021bd3ec2117
owlsn <helloworld.zouhao@gmail.com>
2018-06-19 17:32:10 +0800

    signal

--- spiders/ip_proxy/ip_proxy/spiders/check.py
+++ spiders/ip_proxy/ip_proxy/spiders/check.py
@@ -16,6 +16,20 @@ class CheckSpider(scrapy.Spider):
     allowed_domains = ['www.66ip.cn']
     base_url = 'http://www.66ip.cn/'
 
+    @classmethod
+    def from_crawler(cls, crawler, *args, **kwargs):
+        spider = super(CheckSpider, cls).from_crawler(crawler, *args, **kwargs)
+        crawler.signals.connect(spider.spider_closed, signal=signals.spider_closed)
+        crawler.signals.connect(spider.spider_opened, signal=signals.spider_opened)
+        return spider
+        pass
+    
+    def spider_closed(self., spider, reason):
+        pass
+
+    def spider_opened(self, spider):
+        pass
+
     def start_requests(self):
         for i in range(QUEUE_NUM):
             length = self.conn.llen(QUEUE_KEY + str(i))
