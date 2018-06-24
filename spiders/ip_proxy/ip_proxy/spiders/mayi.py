eb7037fb1054784721ef6ebe5ad38a53b2db6733
owlsn <helloworld.zouhao@gmail.com>
2018-06-24 21:40:05 +0800

    debug mayi

--- spiders/ip_proxy/ip_proxy/spiders/mayi.py
+++ spiders/ip_proxy/ip_proxy/spiders/mayi.py
@@ -4,6 +4,7 @@ from ip_proxy.item.ip_item import IpItem
 from ip_proxy.utils.log import log
 import time
 import json
+import re
 import hashlib
 import requests
 from io import BytesIO
@@ -18,12 +19,18 @@ class MayiSpider(BaseSpider):
     allowed_domains = ['www.mayidaili.com']
     start_urls = [
         'http://www.mayidaili.com/free',
-        'http://www.mayidaili.com/share/'
+        # 'http://www.mayidaili.com/share/'
         ]
 
     def parse(self, response):
-        # logger = log.getLogger('development')
+        logger = log.getLogger('development')
         item = IpItem()
+        # logger.info(response.text)
+        regex = r'proxy_token=[\s\S]{8}'
+        l = re.findall(regex, response.text)
+        proxy_token = l[0][-8:-1] if l else None
+        if not proxy_token:
+            return
         # 解析表格,获取ip数据
         tr_list = response.xpath('//tbody/tr')
         if len(tr_list):
@@ -34,13 +41,16 @@ class MayiSpider(BaseSpider):
                 image_url = image[0].strip() if image else None
                 source = self.base_url
                 if image_url:
+                    print(image_url)
                     header = {
                         'User-Agent' :'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/11.1.1 Safari/605.1.15',
                         'Referer' : 'http://www.mayidaili.com/free',
-                        'Cookie': 'proxy_token=IklhMDPc; Hm_lvt_dad083bfc015b67e98395a37701615ca=1529815110; Hm_lpvt_dad083bfc015b67e98395a37701615ca=1529815110'
+                        'Cookie': 'proxy_token=' + proxy_token,
+                        'Accept': 'image/png,image/svg+xml,image/*;q=0.8,video/*;q=0.8,*/*;q=0.5'
                     }
                     res = requests.get(image_url, headers = header)
-                    print(res)
+                    logger.info('res.text:{}'.format(res.text))
+                    logger.info('res.content:{}'.format(res.content))
                     # text=pytesseract.image_to_string(Image.open(res.content))
                     # print(text)
                 # if ip and image_url:
