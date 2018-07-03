8b2e56f7d7999af3ad1c33e1580a36a60e560a7d
owlsn <helloworld.zouhao@gmail.com>
2018-07-03 17:02:26 +0800

    schedule

--- spiders/ip_proxy/ip_proxy/sched.1.py
+++ spiders/ip_proxy/ip_proxy/sched.1.py
@@ -10,15 +10,6 @@ from ip_proxy.scheduler.crawl import crawler
 from ip_proxy.scheduler.ip_queue import ip_queue
 from twisted.internet import reactor
 
-def job():
-    try:
-        crawler.start()
-        pass
-    except Exception as e:
-        logger = log.getLogger('development')
-        logger.error(traceback.format_exc())
-    pass
-
 def push_ip():
     try:
         ip_queue.do_select()
@@ -28,25 +19,12 @@ def push_ip():
         logger.error(traceback.format_exc())
     pass
 
-# if __name__ == '__main__':
-    # argv = sys.argv
-    # if len(argv) < 2:
-    #     exit()
-    # if argv[1] == 'job':
-    #     job()
-    # elif argv[1] == 'push_ip':
-    #     push_ip()
-
 sched = TwistedScheduler()
 logger = log.getLogger('development')
 crawler.start(sched)
-sched.add_job(job, 'cron', minute='*/1', id='job')
-# sched.add_job(push_ip, 'cron', hour='*/1', id='push_ip')
-  
-logger.info('before the start funciton')
-sched.start()  
+sched.add_job(push_ip, 'cron', hour='*/1', id='push_ip')
+sched.start()
 try:
     reactor.run()
 except (KeyboardInterrupt, SystemExit):
     pass
-logger.info("let us figure out the situation")
