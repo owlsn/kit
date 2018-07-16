ea00e7c600fc1ddbdc5fb956ad55a3b483375799
owlsn <helloworld.zouhao@gmail.com>
2018-07-16 19:12:19 +0800

    asyn

--- display/display/runserver.py
+++ display/display/runserver.py
@@ -1,16 +1,21 @@
 # coding = utf-8
-from tornado import ioloop, web, options, httpserver, template
+from tornado import ioloop, web, options, httpserver, template, gen, httpclient
 from routers import routers
 import os, sys
 p = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(p)
 from display.config import *
 from display.connections.mysql_connection import mysql
+import socket
+import struct
+import json
 
 from tornado.options import define, options
 define('port', default = 8888, help = 'run on the given port', type = int)
 
 class IndexHandler(web.RequestHandler):
+    @web.asynchronous
+    @gen.engine
     def get(self):
         db = self.application.conn
         cursor = db.cursor()
@@ -18,10 +23,21 @@ class IndexHandler(web.RequestHandler):
         params = (0, 10)
         cursor.execute(sql, params)
         res = cursor.fetchall()
+
+        client = httpclient.AsyncHTTPClient()
         li = []
         if res:
             for value in res:
-                data = {'ip' : value[0], 'port' : value[1], 'scheme' : value[2], 'level' : value[3], 'flag':value[4], 'times' : value[5], 'create_time': value[6], 'update_time' : value[7]}
+                ip = socket.inet_ntoa(struct.pack('I',socket.htonl(int(value[0]))))
+                data = {'ip' : ip, 'port' : value[1], 'scheme' : value[2], 'level' : value[3], 'flag':value[4], 'times' : value[5], 'create_time': value[6], 'update_time' : value[7]}
+                response = yield gen.Task(client.fetch, "http://ip.taobao.com/service/getIpInfo.php?ip=" + ip)
+                info = json.loads(response.body.decode('utf-8'))
+                if info['code'] == 0:       
+                    data['isp'] = info['data']['isp']
+                    data['city'] = info['data']['city']
+                    data['area'] = info['data']['area']
+                    data['region'] = info['data']['region']
+                    data['country'] = info['data']['country']
                 li.append(data)
         title = 'ip list'
         items = li
