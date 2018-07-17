43a386081ef4ef0e19ad226d30c1960cee5133c5
owlsn <helloworld.zouhao@gmail.com>
2018-07-17 12:02:04 +0800

    display

--- display/display/runserver.py
+++ display/display/runserver.py
@@ -11,48 +11,95 @@ from display.utils.log import Log
 import socket
 import struct
 import json
+import re
+import requests
 
 from tornado.options import define, options
 define('port', default = 8888, help = 'run on the given port', type = int)
 
 class IndexHandler(web.RequestHandler):
-    @web.asynchronous
-    @gen.engine
     def get(self):
-        key = 'ip_queue_0'
-        d = self.application.redis.lpop(key)
-        logger = Log().getLogger('development')
-        logger.info(d)
+        title = 'index'
+        self.render('index/index.html', title = title)
+
+# class TestHandler(web.RequestHandler):
+#     @web.asynchronous
+#     @gen.engine
+#     def get(self):
+#         title = 'list'
+#         self.render('index/data.html', title = title)
+
+#     def post(self):
+#         key = 'ip_queue_0'
+#         d = self.application.redis.lpop(key)
+#         logger = Log().getLogger('development')
+#         logger.info(d)
+#         db = self.application.conn
+#         cursor = db.cursor()
+#         sql = """select ip, port, scheme, level, flag, times, create_time, update_time  from `ip` order by update_time asc limit %s,%s """
+#         params = (0, 10)
+#         cursor.execute(sql, params)
+#         res = cursor.fetchall()
+
+#         client = httpclient.AsyncHTTPClient()
+#         li = []
+#         if res:
+#             for value in res:
+#                 ip = socket.inet_ntoa(struct.pack('I',socket.htonl(int(value[0]))))
+#                 data = {'ip' : ip, 'port' : value[1], 'scheme' : value[2], 'level' : value[3], 'flag':value[4], 'times' : value[5], 'create_time': value[6], 'update_time' : value[7]}
+#                 response = yield gen.Task(client.fetch, "http://ip.taobao.com/service/getIpInfo.php?ip=" + ip)
+#                 info = json.loads(response.body.decode('utf-8'))
+#                 if info['code'] == 0:       
+#                     data['isp'] = info['data']['isp']
+#                     data['city'] = info['data']['city']
+#                     data['area'] = info['data']['area']
+#                     data['region'] = info['data']['region']
+#                     data['country'] = info['data']['country']
+#                 li.append(data)
+#         return json.dumps(li)
+
+class ListHandler(web.RequestHandler):
+    def get(self):
+        title = 'list'
+        self.render('index/data.html', title = title)
+
+    def post(self):
+        page = self.get_argument('page', 1)
+        limit = self.get_argument('limit', 10)
+        start = self.get_argument('start', 0)
+
         db = self.application.conn
         cursor = db.cursor()
         sql = """select ip, port, scheme, level, flag, times, create_time, update_time  from `ip` order by update_time asc limit %s,%s """
-        params = (0, 10)
+        params = (int(start), int(limit))
+        count_sql = """select count(*) as count  from `ip` """
         cursor.execute(sql, params)
         res = cursor.fetchall()
 
-        client = httpclient.AsyncHTTPClient()
+        r = cursor.execute(count_sql)
+        result = cursor.fetchone()
+        total = result[0]
         li = []
         if res:
             for value in res:
                 ip = socket.inet_ntoa(struct.pack('I',socket.htonl(int(value[0]))))
                 data = {'ip' : ip, 'port' : value[1], 'scheme' : value[2], 'level' : value[3], 'flag':value[4], 'times' : value[5], 'create_time': value[6], 'update_time' : value[7]}
-                response = yield gen.Task(client.fetch, "http://ip.taobao.com/service/getIpInfo.php?ip=" + ip)
-                info = json.loads(response.body.decode('utf-8'))
-                if info['code'] == 0:       
-                    data['isp'] = info['data']['isp']
-                    data['city'] = info['data']['city']
-                    data['area'] = info['data']['area']
-                    data['region'] = info['data']['region']
-                    data['country'] = info['data']['country']
                 li.append(data)
-        title = 'ip list'
-        items = li
-        self.render('index/index.html', title = title, items = items)
+        data = {
+            'page' : page,
+            'start' : start,
+            'limit' : limit,
+            'data' : li,
+            'length' : total
+        }
+        self.write(json.dumps(data))
+
 
 class App(web.Application):
     def __init__(self):
         handers = [
-            (r'/', IndexHandler)
+            (r'/', IndexHandler),
+            (r'/list', ListHandler)
         ]
         settings = dict(
             template_path = TEMPLATES,
@@ -82,3 +129,18 @@ if __name__ == "__main__":
     http_server = httpserver.HTTPServer(App())
     http_server.listen(options.port)
     ioloop.IOLoop.current().start()
+
+# if __name__ == "__main__":
+#     path = ROOT + '/public/static/dist/css/googleapis.fonts.css'
+#     with open(path, 'r') as f:
+#         regex = r'https:\/\/fonts.gstatic.com\/s\/sourcesanspro\/v11\/[\s\S]*?.woff2'
+#         l = re.findall(regex, f.read())
+#         length = len(l)
+#         if length:
+#             for value in l:
+#                 name = value.split('/')
+#                 path = ROOT + '/public/static/dist/fonts/' + name[-1]
+#                 response = requests.get(value)
+#                 if response.status_code == 200:
+#                     with open(path, 'wb') as n:
+#                         n.write(response.content)
