90c0c8a2927bccb20144307fed8bc8ed04452fe0
owlsn <helloworld.zouhao@gmail.com>
2018-07-26 17:00:24 +0800

    debug

--- spiders/ip_proxy/ip_proxy/scheduler/ip_queue.py
+++ spiders/ip_proxy/ip_proxy/scheduler/ip_queue.py
@@ -3,8 +3,9 @@
 # import sys
 # p = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 # sys.path.append(p)
+# from twisted.internet import reactor
 from ip_proxy.connection.redis_connection import redisDb1
-from ip_proxy.connection.mysql_connection import mysqlSyn
+from ip_proxy.connection.mysql_connection import mysqlAsyn
 from ip_proxy.config import QUEUE_NUM
 import traceback
 from ip_proxy.utils.log import Log
@@ -14,48 +15,65 @@ class IpQueue(object):
 
     def __init__(self):
         self.redis = redisDb1.conn
-        self.mysql = mysqlSyn.conn
+        self.dbpool = mysqlAsyn.dbpool
+        self.logger = Log().getLogger('development')
         pass
 
     def getQueue(self, level):
         key = 'ip_queue_' + str(level)
         return key
 
-    def do_select(self):
-        try:
-            logger = Log().getLogger('development')
-            timeArray = time.localtime(time.time())
-            date_time = time.strftime("%Y--%m--%d %H:%M:%S", timeArray)
-            logger.info('ip_queue start at:{}'.format(date_time))
-            
-            for i in range(QUEUE_NUM):
+    def handle_error(self, failure):
+        self.logger.info(str(failure))
+        pass
+
+    def start(self):
+        timeArray = time.localtime(time.time())
+        date_time = time.strftime("%Y--%m--%d %H:%M:%S", timeArray)
+        self.logger.info('ip_queue start at:{}'.format(date_time))
+        for i in range(QUEUE_NUM):
+            try:
                 length = self.redis.llen(self.getQueue(i))
-                print('length:{},i:{}'.format(length, i))
                 if length < 10000:
-                    start = 0
-                    limit = 2500
-                    sql = """select ip, port, scheme, level, flag, times from `ip` where level = %s order by update_time asc limit %s,%s """
-                    while True:
-                        params = (i, start * limit, limit)
-                        cursor = self.mysql.cursor()
-                        cursor.execute(sql, params)
-                        res = cursor.fetchall()
-                        if not res:
-                            break
-                        for value in res:
-                            data = {'ip' : value[0], 'port' : value[1], 'scheme' : value[2], 'level' : value[3], 'flag':value[4], 'times' : value[5]}
-                            if data['level'] is not None:
-                                self.redis.rpush(self.getQueue(data['level']), data)
-                            else:
-                                self.redis.rpush(self.getQueue(0), data)
-                        start = start + 1
-            pass
+                    index = i
+                    res = self.dbpool.runInteraction(self.do_select, index)
+                    res.addErrback(self.handle_error)
+                pass
+            except Exception as e:
+                self.logger.info(traceback.format_exc())
+                pass
+        pass
+
+    def do_select(self, cursor, i):
+        try:
+            length = self.redis.llen(self.getQueue(i))
+            if length < 10000:
+                start = 0
+                limit = 2500
+                sql = """select ip, port, scheme, level, flag, times from `ip` where level = %s order by update_time asc limit %s,%s """
+                while True:
+                    params = (i, start * limit, limit)
+                    cursor.execute(sql, params)
+                    res = cursor.fetchall()
+                    if not len(res):
+                        break
+                    for value in res:
+                        data = {'ip': value['ip'], 'port': value['port'], 'scheme': value['scheme'], 'level': value['level'], 'flag': value['flag'], 'times': value['times']}
+                        if data['level'] is not None:
+                            self.redis.rpush(self.getQueue(data['level']), data)
+                        else:
+                            self.redis.rpush(self.getQueue(0), data)
+                    start = start + 1
         except Exception as e:
-            mysqlSyn.connect()
-            logger = Log().getLogger('development')
-            logger.error(traceback.format_exc())
+            self.logger.error(traceback.format_exc())
             pass
 
 # if __name__ == '__main__':
 #     ip_queue = IpQueue()
-#     ip_queue.do_select()
+#     ip_queue.start()
+#     try:
+#         reactor.run()
+#         pass
+#     except Exception as e:
+#         print('stop')
+#         pass
